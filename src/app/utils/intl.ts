20f70a339afe22eb47efcb6711a6619efd396ac3
sshirey76 <sshirey76@gmail.com>
2025-08-20 16:28:14 -0400

    Translation Key Fix

--- src/app/utils/intl.ts
+++ src/app/utils/intl.ts
@@ -1,14 +1,13 @@
 // Helpers for effectively using the browser's Intl.* tools
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl
 
-import { DimLanguage, browserLangToDimLang } from 'app/i18n';
+import { DimLanguage } from 'app/i18n';
 import memoizeOne from 'memoize-one';
-import { invert } from './collections';
 import { Comparator } from './comparators';
 import { stubTrue } from './functions';
 
-// Our locale names don't line up with the BCP 47 tags for Chinese
-const d2lLangToBrowserLang = invert(browserLangToDimLang);
+// Since we only support English now, we can use a simple mapping
+const d2lLangToBrowserLang = { en: 'en' };
 
 /** Map D2L's locale values to a [BCP 47 language tag](http://tools.ietf.org/html/rfc5646) */
 function mapLocale(language: DimLanguage): Intl.UnicodeBCP47LocaleIdentifier {
